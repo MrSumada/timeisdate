b237a660e4a49b96da75b57c55abf99741473017
MrSumada <ricksamericana.com>
2023-11-02 13:27:10 -0400

    create app, add partyjs, set date functions

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import  Party  from 'party-js';
+
+function App() {
+  const [message, setMessage] = useState("...");
+  const [bgColor, setBgColor] = useState("#94e1b0");
+  const confettiContainer = useRef(null);
+  const [itsTime, setItsTime] = useState(false);
+  const [date, setDate] = useState(10.26);
+  const [time, setTime] = useState(10.27)
+
+  const startConfetti = () => {
+    if (confettiContainer.current) {
+    Party.confetti(confettiContainer.current, {
+      count: 100,  // Number of confetti particles
+      colors: ['#FF0000', '#00FF00', '#0000FF'],  // Array of confetti colors
+    });
+    }
+  }
+
+  useEffect(() => {
+    
+    const timer = setInterval(() => {
+      const newTime = new Date();
+      let newHour = newTime.getHours();
+      if (newHour > 12) { newHour = newHour - 12 };
+      if (newHour === 0) { newHour = 12};
+
+      const newMonth = newTime.getMonth() + 1;
+      const newDay = newTime.getDate();
+      setTime((newHour + newTime.getMinutes()/100).toFixed(2))
+      setDate((newMonth + newDay/100).toFixed(2))
+    }, 1000)
+
+    if (date === time) {
+      setItsTime(true)
+      setMessage("Yup!")
+      const interval = setInterval(() => {
+        setBgColor("#" + (Math.floor((1 - Math.random()/100)*16777215)).toString(16))
+        console.log(bgColor)
+      }, 3000)
+
+      return () => { clearInterval(interval);}
+
+    } else {
+      setItsTime(false);
+      setMessage("Nope");
+    }
+
+    return () => {
+      clearInterval(timer)
+    };
+  })
+
+  useEffect(() => {
+    // Start confetti when your desired state is active
+    if (itsTime) {
+      startConfetti();
+    }
+  }, [itsTime]);
+
+  useEffect(() => {
+    setMessage("...")
+  }, [])
+
+
+  return (
+    <div className="App" ref={confettiContainer} style={{backgroundColor: bgColor}}>
+      <h1>Is the Time also the Date?</h1>
+      <h2>{message}</h2>
+      {/* <p>{date}</p>
+      <p>{time}</p> */}
+    </div>
+  );
+}
+
+export default App;
